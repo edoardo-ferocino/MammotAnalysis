0e750d4df3388f70fff75311836ebdc0314e87ce
Edoardo Ferocino <edoardo.ferocino@polimi.it>
2020-02-18 18:18:06 +0100

    Added request for Temporal Shift in GenerateMultiCurve

--- Objects/SubFunctions/Tool/PerturbativeTools/GenerateMulticurve.m
+++ Objects/SubFunctions/Tool/PerturbativeTools/GenerateMulticurve.m
@@ -9,10 +9,12 @@ y = -(y-LesPos(1)); x = x - LesPos(2);
 Y = Y'; X = X';
 X = X(:).*mtoolobj.Parent.ScaleFactor/10;
 Y = Y(:).*mtoolobj.Parent.ScaleFactor/10;
-answer = inputdlg({'Available thickness','Lesion depth'},'Enter thickness',[1 42],{num2str(Thickness),num2str(Thickness/2)});
-if isempty(answer), message = 'aborted'; return; end
-th=array2table(horzcat(ones(NumData,1),(1:NumData)',zeros(NumData,6),X,Y,repelem(str2double(answer{2}),NumData,1)));
-th.Properties.VariableNames = {'Proc'	'Page'	'Offset'	'Lambda'    'VarShiftInit'	'DetX'	'DetY'	'DetAlpha'	'IncPosX'	'IncPosY'	'IncPosZ'};
+thickness = inputdlg({'Available thickness','Lesion depth'},'Enter thickness',[1 42],{num2str(Thickness),num2str(Thickness/2)});
+if isempty(thickness), message = 'aborted'; return; end
+shift = inputdlg('Temporal shift','Enter shift',[1 42]);
+if isempty(shift), message = 'aborted'; return; end
+th=array2table(horzcat(ones(NumData,1),(1:NumData)',zeros(NumData,2),repelem(str2double(shift{1}),NumData,1),zeros(NumData,3),X,Y,repelem(str2double(thickness{2}),NumData,1)));
+th.Properties.VariableNames = {'Proc'	'Page'	'Offset'	'Lambda'    'ShiftInit'	'DetX'	'DetY'	'DetAlpha'	'IncPosX'	'IncPosY'	'IncPosZ'};
 [FileName,FilePath,FilterIndex]=uiputfilecustom('.multi','Save table');
 if FilterIndex == 0, return, end
 writetable(th,fullfile(FilePath,FileName),'WriteRowNames',false,'WriteVariableNames',true,'FileType','text','Delimiter','tab');
