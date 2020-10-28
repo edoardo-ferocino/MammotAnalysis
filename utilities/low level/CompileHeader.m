4556d37f0a8b1381a4ee6f3be746490a3521db5e
Edoardo Ferocino <edoardo.ferocino@polimi.it>
2020-10-28 22:05:26 +0100

    Added the functionality to sum channels

--- utilities/low level/CompileHeader.m
+++ utilities/low level/CompileHeader.m
@@ -3,40 +3,29 @@ FieldNames = {'Ver','SubHeader','SubHeaderVer','SizeHeader','SizeSubHeader','Siz
     ,'Oma','Date','Time','LoopHome','LoopFirst','LoopLast','LoopDelta','LoopNum','McaChannNum','PageNum'...
     'FrameNum','RamNum','McaTime','McaFactor','MeasNorm','LabelName','LabelContent','Constn','ConstRho'...
     'ConstThick','MammHeader','MammIdxFirst','MammIdxLast','MammIdxTop','MammRateMid','MammRateHigh'};
-C = 1; CT = 1;
-L = 4; LT = 2;
-D = 8; DT = 3;
-S = 2; ST = 4;
-Unit = 1; %byte
-FieldSize = [2*S,L,L,L,L,L,L,L,L,11*C,9*C,3*L,3*L,3*L,3*L,3*L,L,L,L,L,D,D,L,192*C,352*C,D,D,D,L,2*L,2*L,2*L,2*L,2*L]./Unit;
+CT = 1;
+LT = 2;
+DT = 3;
+ST = 4;
 FieldType = [ST,LT,LT,LT,LT,LT,LT,LT,LT,CT,CT,LT,LT,LT,LT,LT,LT,LT,LT,LT,DT,DT,LT,CT,CT,DT,DT,DT,LT,LT,LT,LT,LT,LT];
 
-CompiledHeader = [];
+CompiledHeader = zeros(764,1);
 nfields = numel(FieldNames);
+nstart = 0;
 for iF = 1:nfields
     RawData=CH.(FieldNames{iF});
-    %isCHAR = false;
     switch FieldType(iF)
         case CT
-            if strcmp(FieldNames{iF},'LabelName')
+            if strcmp(FieldNames{iF},'LabelName') || strcmp(FieldNames{iF},'LabelContent')
                 Data = char(RawData); Data = Data'; Data = Data(:);
                 Data=typecast(uint8(Data),'uint8');
-                %isCHAR = true;
-            end
-            if strcmp(FieldNames{iF},'LabelContent')
-                Data = char(RawData); Data = Data'; Data = Data(:);
-                Data=typecast(uint8(Data),'uint8');
-                %isCHAR = true;
             end
             if strcmp(FieldNames{iF},'Date')
-                Data = typecast(uint8(RawData),'uint8');
-                %isCHAR = true;
+                Data =  uint8(horzcat(datestr(RawData,'mm-dd-YYYY'),0));
             end
             if strcmp(FieldNames{iF},'Time')
                 Data = typecast(uint8(RawData),'uint8');
-                %isCHAR = true;
             end
-            %isCHAR = false;
         case LT
             if strcmp(FieldNames{iF},'Kind')
                 switch RawData
@@ -79,13 +68,16 @@ for iF = 1:nfields
         case ST
             if strcmp(FieldNames{iF},'Ver')
                 Data = typecast(int16(RawData(1)),'uint8');
-                Data = [Data typecast(int16(RawData(2)),'uint8')];
+                Data = [Data typecast(int16(RawData(2)),'uint8')]; %#ok<AGROW>
             else
                 Data = typecast(int16(RawData),'uint8');
             end
     end
-    [nr,nc]=size(Data); if nr>nc, Data = Data'; end
-    CompiledHeader = [CompiledHeader; Data'];
+    if isrow(Data)
+        Data = Data';
+    end
+    CompiledHeader(nstart+(1:numel(Data)),1) = Data;
+    nstart = nstart + numel(Data);
 end
 CompiledHeader=cast(CompiledHeader,'double');
 end
